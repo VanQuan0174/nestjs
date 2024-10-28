743ecfbeb66d8b8d83d9536d8f36e67486203f2f
QuanNVph3649 <quanv8786@gmail.com>
2024-10-28 17:57:46 +0700

    thêm mới người dùng, hashPassword

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,11 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Model } from 'mongoose';
+import { User } from './schemas/user.schema';
+import { hashPasswordHelper } from '@/helpers/util';
+import { isEmail } from 'class-validator';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectModel(User.name)
+    private userModel: Model<User>,
+  ) {}
+
+  isEmailExist = async (email: string) => {
+    const isExist = await this.userModel.exists({ email });
+    if (isExist) return true;
+    return false;
+  };
+  async create(createUserDto: CreateUserDto) {
+    const { name, email, password, phone, address, image } = createUserDto;
+    // check email người dùng
+    const checkEmail = await this.isEmailExist(email);
+    if (checkEmail) {
+      throw new BadRequestException(
+        `Email đã tồn tại : ${email} - Vui lòng sử dụng email khác`,
+      );
+    }
+    // hash password
+    const hashPassword = await hashPasswordHelper(password);
+    const user = await this.userModel.create({
+      name,
+      email,
+      password: hashPassword,
+    });
+    return {
+      _id: user._id,
+    };
   }
 
   findAll() {
