96b8f2d1a27938f5f34643cd432c859b95542181
QuanNVph3649 <quanv8786@gmail.com>
2024-10-28 15:21:44 +0700

    tạo base models

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,16 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { Mongoose } from 'mongoose';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schemas/user.schema';
 
 @Module({
-  imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
-  ],
+  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
   controllers: [UsersController],
   providers: [UsersService],
 })
-export class UsersModule {}
-//abc
+export class UsersModule { }
